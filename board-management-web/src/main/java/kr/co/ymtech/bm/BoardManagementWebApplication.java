26deaaa9cb840ff4520f2277d809d0aded555cb5
oneproblemperday <126146421+oneproblemperday@users.noreply.github.com>
2023-08-17 13:50:15 +0900

    project 생성
    프로젝트 기본 생성

---
+++ board-management-web/src/main/java/kr/co/ymtech/bm/BoardManagementWebApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ymtech.bm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardManagementWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardManagementWebApplication.class, args);
+	}
+
+}
---
+++ board-management-web/src/test/java/kr/co/ymtech/bm/BoardManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ymtech.bm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardManagementWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
