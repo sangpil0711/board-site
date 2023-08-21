d7c0ebc9e7209c0467ae00d8b19c7a9113075e1d
oneproblemperday <126146421+oneproblemperday@users.noreply.github.com>
2023-08-21 18:00:27 +0900

    0821
    0821

--- board-management-web/src/main/java/kr/co/ymtech/bm/BoardManagementWebApplication.java
+++ board-management-web/src/main/java/kr/co/ymtech/bm/BoardManagementWebApplication.java
@@ -1,11 +1,12 @@
 package kr.co.ymtech.bm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BoardManagementWebApplication {
 
 		public static void main(String[] args) {
 			SpringApplication.run(BoardManagementWebApplication.class, args);
 		}
-
 	}
--- board-management-web/src/main/resources/static/controller.js
+++ board-management-web/src/main/resources/static/controller.js
@@ -1,10 +1,10 @@
+var app = angular.module('app', []);
 
-
-var app = angular.module('app',[]);
-	app.controller ('controller',function($scope,$filter){
-		$scope.clickfunction=function(){
-			var time =$filter('date')(new Date(),'yyyy-MM-dd HH:mm:ss');
-			$scope.welcome ="Time = "+ time;
-		}
-		
-	});
+app.controller('controller', function($scope, $filter) {
+    $scope.clickfunction = function() {
+        var time = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
+        console.log('hello world');
+        console.log('time', time);
+        $scope.welcome = "Time = " + time;
+    };
+});
