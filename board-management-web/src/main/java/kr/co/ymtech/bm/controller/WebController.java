e3a0bcd0c26d2ccacf24878ea79c6a0571bf8ac7
oneproblemperday <126146421+oneproblemperday@users.noreply.github.com>
2023-08-22 17:19:55 +0900

    0822
    0822

--- board-management-web/src/main/java/kr/co/ymtech/bm/controller/WebController.java
+++ board-management-web/src/main/java/kr/co/ymtech/bm/controller/WebController.java
@@ -19,15 +19,59 @@ public class WebController {
 		return "index";   // "/" 경로로 GET을 요청하면 "index.html" 반환
 	}
 	
+	// Restful API 생성 - 게시판 목록 조회
 	@RequestMapping( value="/board",method = RequestMethod.GET)
 	public ResponseEntity<List<BoardDTO>> getBoard() {
 		List<BoardDTO> list = new ArrayList<>();
 		
 		// #1. board DTO 객체 클래스 파일 생성, 정의
+		// 완료
 		// #2. list에 board DTO 객체 데이터 삽입
-		// #3. 반환
+		BoardDTO dto1 = new BoardDTO(); // 객체 생성
+		dto1.setIndex(1); // 값 세팅
+		dto1.setTitle("Sample Title 1");
+	    dto1.setText("Sample Text 1");
+	    dto1.setUserId("user1");
+	    dto1.setCategory(1);
+	    dto1.setCreateDate(System.currentTimeMillis());
+	    list.add(dto1);
+	    
+		BoardDTO dto2 = new BoardDTO(); // 객체 생성
+		dto2.setIndex(1); // 값 세팅
+		dto2.setTitle("Sample Title 2");
+		dto2.setText("Sample Text 2");
+		dto2.setUserId("user2");
+		dto2.setCategory(1);
+		dto2.setCreateDate(System.currentTimeMillis());
+	    list.add(dto2);
+	    
+		BoardDTO dto3 = new BoardDTO(); // 객체 생성
+		dto3.setIndex(1); // 값 세팅
+		dto3.setTitle("Sample Title 3");
+		dto3.setText("Sample Text 3");
+		dto3.setUserId("user3");
+		dto3.setCategory(1);
+		dto3.setCreateDate(System.currentTimeMillis());
+	    list.add(dto3);
+	    
+	    
+		BoardDTO dto4 = new BoardDTO(); // 객체 생성
+		dto4.setIndex(1); // 값 세팅
+		dto4.setTitle("Sample Title 4");
+		dto4.setText("Sample Text 4");
+		dto4.setUserId("user4");
+		dto4.setCategory(1);
+		dto4.setCreateDate(System.currentTimeMillis());
+	    list.add(dto4);
+	    
+	
 		
-		return new ResponseEntity<List<BoardDTO>>(list, HttpStatus.OK);   // "/" 경로로 GET을 요청하면 "index.html" 반환
+	
+		
+		
+		// #3. 2번에서 만든 데이터 반환
+		
+		return new ResponseEntity<List<BoardDTO>>(list, HttpStatus.OK);   
 	}
 	
 }
