99f3a2d30e5c9df3033b13b282962f716d1a324f
oneproblemperday <126146421+oneproblemperday@users.noreply.github.com>
2023-08-25 09:25:50 +0900

    test
    test

--- board-management-web/src/main/resources/static/factory.js
+++ board-management-web/src/main/resources/static/factory.js
@@ -1,15 +1,3 @@
-var app = angular.module("myApp", ['ngResource']);
-
-		app.factory('BoardFactory', function($resource) {
-			return $resource('/board', null, {
-				query: {
-					method: 'GET',
-					isArray: true,
-					transformResponse: function(data, headersGetter) {
-						var items = angular.fromJson(data);
-						console.log(items);
-						return items;
-					}
-				}
-			});
-		});
+/**
+ * 
+ */
