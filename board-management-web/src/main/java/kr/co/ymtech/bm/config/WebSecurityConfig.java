63435ee57f0e263d1a90ef7e97f5bd8a54ad582a
oneproblemperday <126146421+oneproblemperday@users.noreply.github.com>
2024-01-10 14:05:44 +0900

    0110
    0110

--- board-management-web/src/main/java/kr/co/ymtech/bm/config/WebSecurityConfig.java
+++ board-management-web/src/main/java/kr/co/ymtech/bm/config/WebSecurityConfig.java
@@ -33,9 +33,9 @@ public class WebSecurityConfig {
             .csrf(csrf -> csrf.disable()) // CSRF 보호를 비활성화합니다.
             .authorizeHttpRequests((authorize) -> authorize
                   .antMatchers("/login/**",
-                		       "/user/**",
-                		       "/signup/**",
-                		       "/static/**").permitAll() // 특정 경로에 대한 접근을 허용합니다.
+                             "/user/**",
+                             "/signup/**",
+                             "/static/**").permitAll() // 특정 경로에 대한 접근을 허용합니다.
                   .antMatchers("/fileExplorer/**").hasRole("ADMIN")
                             .anyRequest().authenticated()) // 나머지 요청은 인증이 필요합니다.
             .formLogin(login -> login
